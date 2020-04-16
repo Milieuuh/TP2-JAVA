bb58b050932ac394e1e56e69d243aea18b9efb06
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-16 13:35:55 +0200

    personnel+main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Personnel p = new Personnel("toto","tata",3);
+
+        p.AfficherAge();
     }
 }
--- src/Personnel.java
+++ src/Personnel.java
@@ -1,2 +1,18 @@
 public class Personnel extends Personne {
+
+    //CONSTRUCTOR
+    public Personnel()
+    {
+        super();
+    }
+
+    public Personnel(String nom, String prenom, int age)
+    {
+        super(nom,prenom,age);
+    }
+
+    public void AfficherAge()
+    {
+        System.out.println("Ce personnel a "+ age + "an(s)");
+    }
 }
