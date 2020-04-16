9f3d5f847bf54fb576966195acd0eb9b0515b632
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-16 14:42:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Personne tabPersonne[]=new Personne[5];
+
         //etudiants
         Etudiant e1= new Etudiant("Dupont","Alice",21,"Pascal");
         Etudiant e2= new Etudiant("Dujardin","Benjamin",22,"Ohm");
@@ -13,11 +15,24 @@ public class Main {
 
         //Enseignant
         Enseignant en1 = new Enseignant("Machin", "Boris", 36, "Enseignant", "Informatique", "Electronique");
-        System.out.println(en1.toString());
+       /* System.out.println(en1.toString());
 
         System.out.println(a1.toString());
         a1.ouMeTrouver();
-        e1.ouMeTrouver();
+        e1.ouMeTrouver();*/
 
+       //Tableau des personnes
+        tabPersonne[0]=e1;
+        tabPersonne[1]=e2;
+        tabPersonne[2]=e3;
+        tabPersonne[3]=e4;
+        tabPersonne[4]=a1;
+        tabPersonne[5]=en1;
+
+        for(int i=0;i<tabPersonne.length;i++)
+        {
+            System.out.println(tabPersonne[i].toString());
+            tabPersonne[i].ouMeTrouver();
+        }
     }
 }
