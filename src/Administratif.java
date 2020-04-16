650bbc31a927277778a3ca392b9f76a6abceb5c9
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-16 14:05:59 +0200

    t

--- src/Administratif.java
+++ src/Administratif.java
@@ -1,11 +1,13 @@
 public class Administratif extends Personnel {
+
+
     public Administratif()
     {
         super();
     }
 
-    public Administratif(String nom, String prenom, int age)
+    public Administratif(String nom, String prenom, int age,String service)
     {
-        super(nom,prenom,age);
+        super(nom,prenom,age,service);
     }
 }
