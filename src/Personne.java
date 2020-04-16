104b80f84cccd04dab1dafa84acb2cbff499ab33
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-04-16 13:26:27 +0200

    Update Personne.java

--- src/Personne.java
+++ src/Personne.java
@@ -1,3 +1,21 @@
 public class Personne {
+    //Attributs
+    private String nom;
+    public String prenom;
+    protected int age;
 
+    //Constructeurs
+        public Personne()
+        {
+            nom="Anonyme";
+            prenom="Anonyme";
+            age=-1;
+        }
+
+    public Personne(String nom, String prenom, int age)
+    {
+        this.nom=nom;
+        this.prenom=prenom;
+        this.age=age;
+    }
 }
